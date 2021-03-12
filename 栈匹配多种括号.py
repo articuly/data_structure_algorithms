60175f8923c0a25751b424e9d6dc202a491f8977
articuly <articuly@gmail.com>
2021-03-12 10:28:10 +0800

    栈实现

--- 栈匹配多种括号.py
+++ 栈匹配多种括号.py
@@ -2,6 +2,28 @@
 from 列表实现栈 import Stack
 
 
+def bracket_check(string):
+    s = Stack()
+    balanced = True
+    index = 0
+
+    while index < len(string) and balanced:
+        symbol = string[index]
+        if symbol == '(':
+            s.push(symbol)
+        else:
+            if not s.is_empty():
+                s.pop()
+            else:
+                balanced = False
+        index += 1
+
+    if balanced and s.is_empty():
+        return True
+    else:
+        return False
+
+
 def brackets_checker(string):
     s = Stack()
     balanced = True
@@ -33,6 +55,11 @@ def matches(open, close):
 
 
 if __name__ == '__main__':
+    print(bracket_check('((()))'))
+    print(bracket_check('(())())'))
+    print(bracket_check('()())('))
+    print(bracket_check('()()()'))
+    print('------------')
     print(brackets_checker('(){}[]'))
     print(brackets_checker('{{[()]}}'))
     print(brackets_checker('{[]}(())[]'))
